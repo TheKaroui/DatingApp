d7df41c5a9e44f03ba1aef9a0b5b930a3621a50a
KAROUI <haithem.karoui@capgemini.com>
2019-01-18 14:07:45 +0100

    data service part 2

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  update = false;
+  joke: any;
+
+  constructor(updates: SwUpdate, private data: DataService) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => window.location.reload());
+    });
+  }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      this.joke = res;
+    });
+  }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { NavComponent } from './nav/nav.component';
+
+import { DataService } from './data.service';
 
 @NgModule({
    declarations: [
@@ -17,9 +20,10 @@ import { NavComponent } from './nav/nav.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+      ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+      FormsModule
    ],
-   providers: [],
+   providers: [DataService],
    bootstrap: [AppComponent]
 })
 export class AppModule {}
